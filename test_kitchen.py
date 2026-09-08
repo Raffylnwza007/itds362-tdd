bd021961d1a92ec5e578f47e3d09fa6bb6153f03
Raffy Chaibunma <raffy301258@gmail.com>
2026-09-08 18:10:03 +0700

    A3: green

--- test_kitchen.py
+++ test_kitchen.py
@@ -1,25 +1,25 @@
-# ☐ 200 g × 3 = 600 g
-# ☐ การคูณต้องไม่เปลี่ยนค่าของอ็อบเจ็กต์เดิม
+# ☑ 200 g × 3 = 600 g
+# ☑ การคูณต้องไม่เปลี่ยนค่าของอ็อบเจ็กต์เดิม
 # ☐ ปริมาณสองค่าที่มีทั้งตัวเลขและหน่วยเท่ากันถือว่าเท่ากัน
 # ☐ 1 oz ไม่เท่ากับ 1 g
 # ☐ 200 g + 300 g = 500 g
 # ☐ 200 g + 1 oz แปลงผลลัพธ์เป็นกรัมโดยใช้อัตราแปลงหน่วย
 # ☐ (200 g + 1 oz) × 2
 
-
 from kitchen import Quantity
 
 
 def test_multiplication():
     flour = Quantity(200)
-    flour.times(3)
-    assert flour.amount == 600
+    product = flour.times(3)
+    assert product.amount == 600
 
 
 def test_multiplication_by_two():
     flour = Quantity(200)
-    flour.times(2)
-    assert flour.amount == 400
+    product = flour.times(2)
+    assert product.amount == 400
+
 
 def test_multiplication_returns_a_new_quantity():
     flour = Quantity(200)
