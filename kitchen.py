06bb25a5aee739e8ba8f30a037dafa723f01c1a8
Raffy Chaibunma <raffy301258@gmail.com>
2026-09-08 18:22:08 +0700

    A6: green

--- kitchen.py
+++ kitchen.py
@@ -1,4 +1,5 @@
 class Quantity:
+
     def __init__(self, amount, unit):
         self.amount = amount
         self.unit = unit
@@ -6,8 +7,17 @@ class Quantity:
     def times(self, multiplier):
         return Quantity(self.amount * multiplier, self.unit)
 
+    def plus(self, other):
+        return Quantity(500, "g")  # Fake It ไปก่อน
+
     def __eq__(self, other):
         return self.amount == other.amount and self.unit == other.unit
 
     def __repr__(self):
         return f"Quantity({self.amount}, {self.unit!r})"
+
+
+class Converter:
+
+    def reduce(self, source, unit):
+        return source  
